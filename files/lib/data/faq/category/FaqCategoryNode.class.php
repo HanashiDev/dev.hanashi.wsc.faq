506975b94e3c3cc9e59bce78647abca75c3a8968
Peter Lohse <peter.lohse@fsd-web.de>
2020-05-11 17:09:46 +0200

    category permissions added

---
+++ files/lib/data/faq/category/FaqCategoryNode.class.php
@@ -0,0 +1,10 @@
+<?php
+namespace wcf\data\faq\category;
+use wcf\data\category\CategoryNode;
+
+class FaqCategoryNode extends CategoryNode {
+	/**
+	 * @inheritDoc
+	 */
+	protected static $baseClass = FaqCategory::class;
+}
---
+++ files/lib/data/faq/category/FaqCategoryNodeTree.class.php
@@ -0,0 +1,18 @@
+<?php
+namespace wcf\data\faq\category;
+use wcf\data\category\CategoryNode;
+use wcf\data\category\CategoryNodeTree;
+
+class FaqCategoryNodeTree extends CategoryNodeTree {
+	/**
+	 * @inheritDoc
+	 */
+	protected $nodeClassName = FaqCategoryNode::class;
+	
+	/**
+	 * @inheritDoc
+	 */
+	public function isIncluded(CategoryNode $categoryNode) {
+		return parent::isIncluded($categoryNode) && $categoryNode->isAccessible();
+	}
+}
