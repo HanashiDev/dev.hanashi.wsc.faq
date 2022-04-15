d06d938acd993bef4708648d70c9e91b67e126bf
Hanashi <hanashi@hanashi.eu>
2022-04-15 10:42:47 +0200

    uniform indentation

--- files/js/3rdParty/redactor2/plugins/FaqQuestion.js
+++ files/js/3rdParty/redactor2/plugins/FaqQuestion.js
@@ -3,7 +3,7 @@ $.Redactor.prototype.FaqQuestion = function() {
 	
 	return {
 		init: function() {
-            const button = this.button.add('faqQuestion', '');
+			const button = this.button.add('faqQuestion', '');
 			
 			require(['TKirch/Redactor/Faq/Question'], ({ RedactorFaqQuestion }) => {
 				new RedactorFaqQuestion(this, button[0]);
