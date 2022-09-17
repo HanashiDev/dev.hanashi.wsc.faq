ae7170aefb0db0d5317744818b85469c2675bd50
Hanashi <hanashi@hanashi.eu>
2022-09-17 17:00:52 +0200

    Buildscript korrigiert

--- make.sh
+++ make.sh
@@ -12,7 +12,7 @@ do
 done
 
 rm -rf ${PACKAGE_NAME}.tar ${PACKAGE_NAME}.tar.gz
-7z a -ttar -mx=9 ${PACKAGE_NAME}.tar ./* -x!acptemplates -x!files -x!templates -x!${PACKAGE_NAME}.tar -x!${PACKAGE_NAME}.tar.gz -x!make.sh -x!make.bat -x!ts -x!node_modules -x!package-lock.json -x!package.json -x!tsconfig.json -x!README.md -x!.* -x!README.md
+7z a -ttar -mx=9 ${PACKAGE_NAME}.tar ./* -x!acptemplates -x!files -x!templates -x!${PACKAGE_NAME}.tar -x!${PACKAGE_NAME}.tar.gz -x!make.sh -x!make.bat -x!ts -x!node_modules -x!package-lock.json -x!package.json -x!tsconfig.json -x!README.md -x!.*
 
 for i in "${PACKAGE_TYPES[@]}"
 do
